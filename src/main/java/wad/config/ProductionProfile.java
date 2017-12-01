78075f48c03f5c5487f51a8ec71f9ff0d811a2ef
Matias Isosaari <matias_isosaari@hotmail.com>
2017-12-01 12:21:28 +0200

    datasource

--- src/main/java/wad/config/ProductionProfile.java
+++ src/main/java/wad/config/ProductionProfile.java
@@ -1,19 +1,26 @@
 package wad.config;
 
-import javax.activation.DataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 @Configuration
 @Profile("production")
 public class ProductionProfile {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return (DataSource) DataSourceBuilder.create().build();
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
     }
 }
